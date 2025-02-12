46c2dc6f6d2877c61c5b3163386227ed7a672a27
Developer YLabz <developer@ylabz.com>
2025-02-11 20:47:47 -0500

    clean up code

--- app/src/main/java/com/example/bike/MySpatialContent.kt
+++ app/src/main/java/com/example/bike/MySpatialContent.kt
@@ -18,33 +18,7 @@ import androidx.xr.compose.subspace.layout.resizable
 import androidx.xr.compose.subspace.layout.size
 import androidx.xr.compose.unit.DpVolumeSize
 
-@SuppressLint("RestrictedApi")
-@Composable
-fun MySpatialContent(onRequestHomeSpaceMode: () -> Unit) {
-    SpatialPanel(
-        modifier = SubspaceModifier
-            .size(DpVolumeSize(width = 1280.dp, height = 800.dp, depth = 7.dp))
-            .resizable()
-            .movable()
-    ) {
-        // Display the 3D model over the entire spatial panel.
-        HoverBikeModelViewer(modifier = Modifier.fillMaxSize())
 
-        // Overlay a mode-switch button using the Orbiter.
-        Orbiter(
-            position = OrbiterEdge.Top,
-            offset = EdgeOffset.inner(offset = 20.dp),
-            alignment = androidx.compose.ui.Alignment.End,
-            //shape = SpatialRoundedCornerShape(CornerSize(28.dp))
-        ) {
-            HomeSpaceModeIconButton(
-                onClick = onRequestHomeSpaceMode,
-                modifier = Modifier
-                    .fillMaxSize() // or use a fixed size, e.g. Modifier.size(56.dp)
-            )
-        }
-    }
-}
 
 @Composable
 fun CornerSize(x0: Dp) {
